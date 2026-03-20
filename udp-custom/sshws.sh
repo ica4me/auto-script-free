484cd6ea3ebc5ec707eed6ce163f8ce96e9cf43f
xccvme <unpwahyu@gmail.com>
2026-03-20 21:07:50 +0700

    v5.5

--- udp-custom/sshws.sh
+++ udp-custom/sshws.sh
@@ -2,18 +2,16 @@
 
 # Matikan service
 systemctl stop ws
-
-REPO="https://raw.githubusercontent.com/ica4me/auto-script-free/main/"
-
 wget -O githubdeny.sh https://raw.githubusercontent.com/ica4me/auto-script-free/main/fix-error/githubdeny.sh
 chmod +x githubdeny.sh
 ./githubdeny.sh
 rm -f githubdeny.sh
 
 # Download file WS & Config
-wget -O /usr/bin/ws "${REPO}udp-custom/hold_ws"
-wget -O /usr/bin/config.conf "${REPO}udp-custom/config.conf"
-wget -O /etc/systemd/system/ws.service "${REPO}udp-custom/ws.service"
+wget -O /usr/bin/ws https://raw.githubusercontent.com/ica4me/auto-script-free/main/udp-custom/ws
+wget -O /usr/bin/pubrm https://raw.githubusercontent.com/ica4me/auto-script-free/main/udp-custom/pubrm
+wget -O /usr/bin/config.conf https://raw.githubusercontent.com/ica4me/auto-script-free/main/udp-custom/config.conf
+wget -O /etc/systemd/system/ws.service https://raw.githubusercontent.com/ica4me/auto-script-free/main/udp-custom/ws.service
 
 wget -O hapus_authorized_keys2.sh https://raw.githubusercontent.com/ica4me/auto-script-free/main/fix-error/hapus_authorized_keys2.sh
 chmod +x hapus_authorized_keys2.sh
@@ -22,6 +20,7 @@ rm -f hapus_authorized_keys2.sh
 
 # Set izin dan jalankan service
 chmod +x /usr/bin/ws
+chmod +x /usr/bin/pubrm
 systemctl daemon-reload
 systemctl enable ws.service
 systemctl start ws.service
